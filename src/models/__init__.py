d489e74f39027556c2c8033793b9148b050bdad1
Mert Kurttutan <kurttutan.mert@gmail.com>
2023-02-11 10:54:00 +0100

    Add improve classifier option

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,12 +1,29 @@
 from .base_cnn import BaseCNN
 import timm
+from torch import nn
+
+
+def get_classifier_head(in_dim, out_dim):
+    return nn.Sequential(
+        nn.BatchNorm1d(in_dim),
+        nn.Linear(in_features=in_dim, out_features=512, bias=False),
+        nn.ReLU(inplace=True),
+        nn.BatchNorm1d(512),
+        nn.Dropout(0.4),
+        nn.Linear(in_features=512, out_features=out_dim, bias=False)
+    )
 
 
 def get_model(config):
 
-    
     if config["model_name"] == "base_cnn":
         return BaseCNN(config)
     else:
-        return timm.create_model(**config)
+        improve_classifier = config.pop("improve_classifier")
+        model = timm.create_model(**config)
+        if improve_classifier:
+            in_dim = model.get_classifier().in_features
+            out_dim = model.get_classifier().out_features
+            model.fc = get_classifier_head(in_dim, out_dim)
+        return model
 
